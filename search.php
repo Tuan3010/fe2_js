6814fc20573ffe6fb13332863e35b3ccb9838664
Tuan3010 <tuanly.0234@gmail.com>
2025-01-02 17:27:51 +0700

    commit A

--- search.php
+++ search.php
@@ -27,6 +27,7 @@ $categoryList = $categoryModel->getAllCategories();
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
+            <p>This is Commit A</p>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
